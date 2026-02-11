7c3cbaa597fa7ea31a63893413479d8234c004f8
abhii-creep-hub <abhinav.sharma_cs.aiml24@gla.ac.in>
2026-02-11 09:44:41 +0530

    ADDED LIBRARIES IN platform.ini

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <Arduino.h>
+#include <wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR    0X3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+        Serial.println(F("OLED NOT FOUND"));
+        while (true);
+    }
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.println("Arduino OLED Test");
+    display.println("OLED with I2C");
+    display.display();
+
+
 }
 
 void loop() {
-// write your code here
+    // write your code here
 }
